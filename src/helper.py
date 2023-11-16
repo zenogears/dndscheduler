2ae5461fecddfeeaa09a9cf2b4980085f75fab28
Chibiko <zenogears.aka@gmail.com>
2023-11-16 21:20:38 +0300

    ver 0.2

--- src/helper.py
+++ src/helper.py
@@ -1,16 +1,150 @@
 import os
 
 from crontab import CronTab
+from configparser import ConfigParser
 
-def main():
-    cfg = {
+CONFIG_DIR=f"{os.path.expanduser('~')}/.config/dndscheduler"
+CONFIG_FILE=f'sleep.ini'
+
+ALLDAYS = {"MON": "Понедельник",
+        "TUE": "Вторник",
+        "WED": "Среда",
+        "THU": "Четверг",
+        "FRI": "Пятница",
+        "SAT": "Суббота",
+        "SUN": "Воскресенье"
+        }
+
+default_cfg = {
     "main": {
-        "HOUR": 22,
-        "MINUTE": 45,
-        "HERO": 7
+        "SHOUR": 23,
+        "SMIN": 0,
+        "EHOUR": 7,
+        "EMIN": 0,
+        "MON": False,
+        "TUE": False,
+        "WED": False,
+        "THU": False,
+        "FRI": False,
+        "SAT": False,
+        "SUN": False,
+        "SILENT": True,
+        "ENABLED": False
     }}
+
+def job_status(data):
+    cfg = main_func()
+    if eval(cfg["main"][data]):
+         return({"status": True, "text": "Включено"})
+    else:
+         return({"status": False, "text": "Выключено"})
+
+def ret_days():
+    dayson = []
+    cfg = main_func()
+    workdays = ["MON", "TUE", "WED", "THU", "FRI"]
+    weekends = ["SAT","SUN"]
+    alldays = workdays.copy()
+    alldays.extend(weekends)
+    for i in alldays:
+        if eval(cfg["main"][i]):
+            dayson.append(i)
+    if len(dayson) == 7:
+        return('Everyday')
+    if dayson == workdays:
+        return('On workdays')
+    if dayson == weekends:
+        return('On weekends')
+    return(', '.join(dayson) if dayson else 'Never')
+            
+
+def set_status(param, value):
+    cfg = load_data()
+    cfg["main"][param] = str(value)
+    save_data(cfg)
+    if eval(cfg["main"]["ENABLED"]) or param.lower() == "enabled":
+        create_cron()
+    return True
+
+def save_data(cfg):
+    with open(f'{CONFIG_DIR}/{CONFIG_FILE}', 'w') as f:
+        cfg.write(f)
+    
+def load_data():
+    cfg_default = ConfigParser()
+    cfg_default["main"] = default_cfg["main"]
+    cfg = ConfigParser()
+    cfg.read(f'{CONFIG_DIR}/{CONFIG_FILE}')
+    for i in cfg['main']:
+        cfg_default["main"][i] = cfg["main"][i]
+    return cfg_default
+
+def create_cron():
+    cfg = load_data()
+    dayofweeks = [x for x in ALLDAYS if cfg["main"][x] == 'True']
+    statusDict = {"true": "quiet", "false": "full", "silent": "silent"}
+    bannersDict = {"true": "false", "false": "true"}
+    status = statusDict[str(cfg["main"]["ENABLED"]).lower()] 
+    status_banners = bannersDict[str(cfg["main"]["ENABLED"]).lower()]
+    cron = CronTab(user=True)
+    
+    job_banners_on = next(cron.find_comment('BannersOn'), False)
+    if not job_banners_on:
+        command_banners_on = f'gsettings set org.gnome.desktop.notifications show-banners true'
+        job_banners_on = cron.new(command=command_banners_on, comment='BannersOn')
+    
+    job_banners_off = next(cron.find_comment('BannersOff'), False)
+    if not job_banners_off:
+        command_banners_off = f'gsettings set org.gnome.desktop.notifications show-banners false'
+        job_banners_off = cron.new(command=command_banners_off, comment='BannersOff')
+        
+    job_profile_on = next(cron.find_comment('ProfileOn'), False)
+    if not job_profile_on:
+        command_profile_on = f'gsettings set org.sigxcpu.feedbackd profile full'
+        job_profile_on = cron.new(command=command_profile_on, comment='ProfileOn')
+        
+    job_profile_off = next(cron.find_comment('ProfileOff'), False)
+    if not job_profile_off:
+        command_profile_off = f'gsettings set org.sigxcpu.feedbackd profile quiet'
+        job_profile_off = cron.new(command=command_profile_off, comment='ProfileOff')
+   
+    job_banners_on.enable(eval(cfg["main"]["ENABLED"]))
+    job_banners_off.enable(eval(cfg["main"]["ENABLED"]))
+    
+    job_banners_on.hour.on(cfg["main"]["EHOUR"])
+    job_banners_on.minute.on(cfg["main"]["EMIN"])
+    job_banners_off.hour.on(cfg["main"]["SHOUR"])
+    job_banners_off.minute.on(cfg["main"]["SMIN"])
+    
+    job_profile_on.enable(eval(cfg["main"]["ENABLED"]))
+    job_profile_off.enable(eval(cfg["main"]["ENABLED"]))
+    
+    job_profile_on.hour.on(cfg["main"]["EHOUR"])
+    job_profile_on.minute.on(cfg["main"]["EMIN"])
+    job_profile_off.hour.on(cfg["main"]["SHOUR"])
+    job_profile_off.minute.on(cfg["main"]["SMIN"])
+
+    if dayofweeks:
+        job_banners_on.dow.on(*dayofweeks)
+        job_banners_off.dow.on(*dayofweeks)
+        job_profile_on.dow.on(*dayofweeks)
+        job_profile_off.dow.on(*dayofweeks)
+
+    cron.write()
+
+    
+
+def main_func():
+    cfg = ConfigParser()
+    if not os.path.isdir(CONFIG_DIR):
+        os.mkdir(CONFIG_DIR)
+    if not os.path.exists(f'{CONFIG_DIR}/{CONFIG_FILE}'):
+        cfg["main"] = default_cfg["main"]
+        save_data(cfg)
+    else:
+        cfg = load_data()
     return cfg
 
 
 if __name__=='__main__':
-    main()
+    main_func()
--- src/main.py
+++ src/main.py
@@ -6,12 +6,14 @@ gi.require_version('Adw', '1')
 
 from gi.repository import Gtk, Adw, Gio
 
-from .helper import main
+from .helper import main_func, job_status, set_status, ALLDAYS, ret_days
 
+def timecreators(timech):
+    return(''.join([str(0), str(timech)]) if len(timech) < 2 else timech)
 
 class MainWindow(Gtk.ApplicationWindow):
     def __init__(self, *args, **kwargs):
-        cfg = main()
+        cfg = main_func()
         super().__init__(*args, **kwargs)
         self.set_default_size(360, 720)
         self.set_title("DND Scheduler")
@@ -22,16 +24,25 @@ class MainWindow(Gtk.ApplicationWindow):
         self.box.set_margin_end(10)
         self.set_child(self.box)
 
+        self.switch_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+
+
         self.label_from = Gtk.Label(label="Не беспокоить с:")
-        self.button_from = Gtk.Button(label=f"{cfg['main']['HOUR']}:00")
+        self.button_from = Gtk.Button(label=f"{timecreators(cfg['main']['SHOUR'])}:{timecreators(cfg['main']['SMIN'])}")
 
         self.label_to = Gtk.Label(label="Не беспокоить до:")
-        self.button_to = Gtk.Button(label="10:00 следующего дня")
+        self.button_to = Gtk.Button(label=f"{timecreators(cfg['main']['EHOUR'])}:{timecreators(cfg['main']['EMIN'])}")
 
         self.label_repeat = Gtk.Label(label="Повтор:")
-        self.button_repeat = Gtk.Button(label="Пн, Вт, Ср, Чт, Пт")
+        self.button_repeat = Gtk.Button(label=f"{ret_days()}")
 
         self.button_about = Gtk.Button(label="О приложении")
+
+        self.label_onoff = Gtk.Label(label=f"Параметры запуска: {job_status('ENABLED')['text']}")
+        self.switch_onoff = Gtk.Switch()
+        self.switch_onoff.set_active(job_status("ENABLED")["status"])
+        self.switch_onoff.connect("state-set", self.switch_switched, "ENABLED")
+        self.switch_box.append(self.switch_onoff)
 
         self.box.append(self.label_from)
         self.box.append(self.button_from)
@@ -42,6 +53,9 @@ class MainWindow(Gtk.ApplicationWindow):
         self.box.append(self.label_repeat)
         self.box.append(self.button_repeat)
 
+        self.box.append(self.label_onoff)
+        self.box.append(self.switch_box)
+
         self.box.append(self.button_about)
 
         self.button_from.connect('clicked', self.bfrom)
@@ -50,39 +64,161 @@ class MainWindow(Gtk.ApplicationWindow):
         self.button_about.connect('clicked', self.show_about)
 
     def brepeat(self, brepeat):
-        before_form = Gtk.ApplicationWindow(transient_for=app.get_active_window())
-        before_form.set_default_size(360, 720)
-        before_form.set_title("Время начала")
-        before_form.show()
+        self.brepeat_form = Gtk.ApplicationWindow(transient_for=app.get_active_window())
+        self.brepeat_form.set_default_size(360, 720)
+        self.brepeat_form.set_title("Повторять по дням:")
+        self.brepeat_form.show()
+
+        self.box_repeat = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        self.box_repeat.set_margin_top(20)
+        self.box_repeat.set_margin_start(10)
+        self.box_repeat.set_margin_end(10)
+        self.brepeat_form.set_child(self.box_repeat)
+
+        for day in ALLDAYS:
+            self.box_day = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+            self.label_day = Gtk.Label(label=f"{ALLDAYS[day]}:", halign=Gtk.Align.START)
+            self.label_day.set_hexpand(True)
+            self.switch_day = Gtk.Switch(halign=Gtk.Align.CENTER)
+            self.switch_day.set_active(job_status(day)["status"])
+            self.switch_day.connect("state-set", self.switch_func, day)
+            self.box_day.append(self.label_day)
+            self.box_day.append(self.switch_day)
+            self.box_repeat.append(self.box_day)
+
+        self.button_repeat_close = Gtk.Button.new_with_mnemonic("_SAVE")
+        self.button_repeat_close.connect("clicked", self.on_close_clicked, self.brepeat_form)
+        self.box_repeat.append(self.button_repeat_close)
+
+    def switch_func(self, switch, state, param):
+        set_status(param, state)
+        self.button_repeat.set_label(ret_days())
 
     def bfrom(self, bfrom):
-        before_form = Gtk.ApplicationWindow(transient_for=app.get_active_window())
-        before_form.set_default_size(360, 720)
-        before_form.set_title("Время начала")
-        before_form.show()
+        cfg = main_func()
+        self.before_form = Gtk.ApplicationWindow(transient_for=app.get_active_window())
+        self.before_form.set_default_size(360, 720)
+        self.before_form.set_title("Время начала")
+        self.before_form.show()
+
+        self.box_from = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+
+        self.before_form.set_child(self.box_from)
+        self.label_from = Gtk.Label(label = 'Выставим часы:')
+        self.label_from_min = Gtk.Label(label = 'Выставим минуты:')
+
+        self.hour_store = Gtk.ListStore(str)
+        time = [x for x in range(0,25)]
+        for hour in time:
+            self.hour_store.append([str(hour)])
+
+        self.min_store = Gtk.ListStore(str)
+        mtime = [x for x in range(0,60, 5)]
+        for minute in mtime:
+            self.min_store.append([str(minute)])
+
+        self.hour_combo = Gtk.ComboBox.new_with_model_and_entry(self.hour_store)
+        self.hour_combo.connect("changed", self.on_time_combo_changed, "SHOUR")
+        self.hour_combo.set_entry_text_column(0)
+        self.hour_combo.set_active(int(cfg['main']['SHOUR']))
+
+        self.min_combo = Gtk.ComboBox.new_with_model_and_entry(self.min_store)
+        self.min_combo.connect("changed", self.on_time_combo_changed, "SMIN")
+        self.min_combo.set_entry_text_column(0)
+
+        self.min_combo.set_active(mtime.index(int(cfg['main']['SMIN'])))
+
+        self.button_from_close = Gtk.Button.new_with_mnemonic("_SAVE")
+        self.button_from_close.connect("clicked", self.on_close_clicked, self.before_form)
+
+        self.box_from.append(self.label_from)
+        self.box_from.append(self.hour_combo)
+        self.box_from.append(self.label_from_min)
+        self.box_from.append(self.min_combo)
+        self.box_from.append(self.button_from_close)
+
+    def on_close_clicked(self, button, window):
+        window.destroy()
+
+
+    def on_time_combo_changed(self, combo, param):
+        cfg = main_func()
+        if combo.get_model()[combo.get_active_iter()][0] is not None:
+            data = combo.get_model()[combo.get_active_iter()][0]
+            if set_status(param, data):
+                if 'shour' == param.lower():
+                    self.button_from.set_label(f"{timecreators(data)}:{timecreators(cfg['main']['SMIN'])}")
+                if 'smin' == param.lower():
+                    self.button_from.set_label(f"{timecreators(cfg['main']['SHOUR'])}:{timecreators(data)}")
+                if 'ehour' == param.lower():
+                    self.button_to.set_label(f"{timecreators(data)}:{timecreators(cfg['main']['EMIN'])}")
+                if 'emin' == param.lower():
+                    self.button_to.set_label(f"{timecreators(cfg['main']['EHOUR'])}:{timecreators(data)}")
 
     def bto(self, bto):
-        before_form = Gtk.ApplicationWindow(transient_for=app.get_active_window())
-        before_form.set_default_size(360, 720)
-        before_form.set_title("Время окончания")
-        before_form.show()
+        cfg = main_func()
+        self.to_form = Gtk.ApplicationWindow(transient_for=app.get_active_window())
+        self.to_form.set_default_size(360, 720)
+        self.to_form.set_title("Время окончания")
+        self.to_form.show()
+
+        self.box_to = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+
+        self.to_form.set_child(self.box_to)
+        self.label_to = Gtk.Label(label = 'Выставим часы:')
+        self.label_to_min = Gtk.Label(label = 'Выставим минуты:')
+
+        self.to_hour_store = Gtk.ListStore(str)
+        time = [x for x in range(0,25)]
+        for hour in time:
+            self.to_hour_store.append([str(hour)])
+
+        self.to_min_store = Gtk.ListStore(str)
+        mtime = [x for x in range(0,60, 5)]
+        for minute in mtime:
+            self.to_min_store.append([str(minute)])
+
+        self.to_hour_combo = Gtk.ComboBox.new_with_model_and_entry(self.to_hour_store)
+        self.to_hour_combo.connect("changed", self.on_time_combo_changed, "EHOUR")
+        self.to_hour_combo.set_entry_text_column(0)
+        self.to_hour_combo.set_active(int(cfg['main']['EHOUR']))
+
+        self.to_min_combo = Gtk.ComboBox.new_with_model_and_entry(self.to_min_store)
+        self.to_min_combo.connect("changed", self.on_time_combo_changed, "EMIN")
+        self.to_min_combo.set_entry_text_column(0)
+        self.to_min_combo.set_active(mtime.index(int(cfg['main']['EMIN'])))
+
+        self.button_to_close = Gtk.Button.new_with_mnemonic("_SAVE")
+        self.button_to_close.connect("clicked", self.on_close_clicked, self.to_form)
+
+        self.box_to.append(self.label_to)
+        self.box_to.append(self.to_hour_combo)
+        self.box_to.append(self.label_to_min)
+        self.box_to.append(self.to_min_combo)
+        self.box_to.append(self.button_to_close)
+
 
     def show_about(self, action=None, param=None):
-        dialog = Adw.AboutWindow(transient_for=app.get_active_window())
-        dialog.set_default_size(360, 720)
-        dialog.set_application_name("DND Scheduler")
-        dialog.set_version("0.1")
-        dialog.set_developer_name("Chibiko")
-        dialog.set_license_type(Gtk.License(Gtk.License.GPL_3_0))
-        dialog.set_comments("Application to rule schedule for Librem 5")
-        dialog.set_website("https://github.com/zenogears/dndscheduler")
-        dialog.set_issue_url("https://github.com/zenogears/dndscheduler/issues")
-        dialog.add_credit_section("Contributors", ["Chibiko"])
-        dialog.set_translator_credits("Chibiko")
-        dialog.set_copyright("© 2023 Chibiko")
-        dialog.set_developers(["Chibiko"])
-        dialog.set_application_icon("org.gnome.dndscheduler")
-        dialog.show()
+        self.dialog = Adw.AboutWindow(transient_for=app.get_active_window())
+        self.dialog.set_default_size(360, 720)
+        self.dialog.set_application_name("DND Scheduler")
+        self.dialog.set_version("0.2")
+        self.dialog.set_developer_name("Chibiko")
+        self.dialog.set_license_type(Gtk.License(Gtk.License.GPL_3_0))
+        self.dialog.set_comments("Application to rule schedule for Librem 5")
+        self.dialog.set_website("https://github.com/zenogears/dndscheduler")
+        self.dialog.set_issue_url("https://github.com/zenogears/dndscheduler/issues")
+        self.dialog.add_credit_section("Contributors", ["Chibiko"])
+        self.dialog.set_translator_credits("Chibiko")
+        self.dialog.set_copyright("© 2023 Chibiko")
+        self.dialog.set_developers(["Chibiko"])
+        self.dialog.set_application_icon("org.gnome.dndscheduler")
+        self.dialog.show()
+
+    def switch_switched(self, switch, state, param):
+        set_status(param, state)
+        self.label_onoff.set_text(f"Параметры запуска: {job_status('ENABLED')['text']}")
+
 
 class MyApp(Adw.Application):
     def __init__(self, **kwargs):
@@ -95,4 +231,6 @@ class MyApp(Adw.Application):
 
 app = MyApp(application_id="org.gnome.dndscheduler")
 app.run(sys.argv)
+sm = app.get_style_manager()
+sm.set_color_scheme(Adw.ColorScheme.PREFER_DARK)
 
